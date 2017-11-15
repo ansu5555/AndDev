bf84d50d83decb0427eaf9a10b93caaea46a8977
Ansuman <ansuman5555@gmail.com>
2017-11-16 00:39:05 +0530

    added form control for edit and delete

--- things2do/todo/staticfiles/js/cust_todo.js
+++ things2do/todo/staticfiles/js/cust_todo.js
@@ -20,22 +20,24 @@ $(document).ready(function(){
             $(this).closest("tr").css("background-color", "#ffffff");
             $(this).children("span").removeClass("fa-check-square-o");
             $(this).children("span").addClass("fa-square-o");
+            $(this).closest("tr").find("#Selector").val("edt");
+            $(this).closest("tr").find("#Value").val("False");
         }
         else {
             $(this).next("td").wrap("<del></del>");
             $(this).closest("tr").css("background-color", "#cccccc");
             $(this).children("span").removeClass("fa-square-o");
             $(this).children("span").addClass("fa-check-square-o");
+            $(this).closest("tr").find("#Selector").val("edt");
+            $(this).closest("tr").find("#Value").val("True");
         }
 
-//        $(this).children(".fa fa-square-o").attr("fa fa-square-o" "fa fa-check-square-o");
-//        $(this).children(".writeup").unwrap();
-//        $(this).children(".fa fa-check-square-o").attr("fa fa-check-square-o" "fa fa-square-o");
     });
 
 
     $("td[data-delname]").click(function(){
         $(this).closest("tr").remove();
+        $(this).closest("tr").find("#Selector").val("del");
     });
 
 
